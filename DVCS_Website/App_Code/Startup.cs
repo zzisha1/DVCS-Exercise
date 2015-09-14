3fe93a3596f4c7bb2934c5447ba022ffdc094abf
zzisha1 <zisha_cse2008@yahoo.com>
2015-09-14 16:37:31 -0600

    created DVCS exercise

---
+++ DVCS_Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DVCS_Website.Startup))]
+namespace DVCS_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
